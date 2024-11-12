59da6a2800b2f634bdee5c9c3caab83e92419996
Darcy Ye <darcyye@silverhand.io>
2024-11-12 19:13:22 +0800

    chore: update comments

--- src/Logto.AspNetCore.Authentication/LogtoParameters.cs
+++ src/Logto.AspNetCore.Authentication/LogtoParameters.cs
@@ -150,7 +150,7 @@ public static class LogtoParameters
 
     /// <summary>
     /// The identifiers to use for authentication.
-    /// Should work with <see cref="FirstScreen"/>.
+    /// This parameter MUST be used together with <see cref="FirstScreen"/>.
     /// </summary>
     public static class Identifiers
     {
